c5ec752f68c08411815551ea487a24993f59a9ff
ashishnagarse <ashish.nagarse.2202@gmail.com>
2025-07-10 12:19:03 +0530

    feat: React app Intro

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Welcome from "./Welcome";
+
+function App() {
+  return(
+    <div>
+      <Welcome name="Alice"/>
+      <Welcome name="Bob"/>
+      <Welcome name="Charlie"/>
+    </div>
+  );
+}
+
+export default App;
